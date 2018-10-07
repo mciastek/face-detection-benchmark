5113ce31accad01ce8f661b2ddd6eefc8234bd0e
Mirosław Ciastek <mirek@swingdev.io>
2018-10-07 12:07:32 +0200

    Add favicon. Update meta data.

--- meta-data.js
+++ meta-data.js
@@ -3,9 +3,9 @@ const siteUrl = process.env.WEBSITE_BASE_URL
 module.exports = {
   SITE_URL: process.env.WEBSITE_BASE_URL,
   TITLE: 'Face Detection Benchmark',
-  OG_COVER: `${siteUrl}/og-cover.jpg`,
-  OG_COVER_TWITTER: `${siteUrl}/og-cover-twitter.jpg`,
-  TWITTER_HANDLE: '@swingdevio',
+  OG_COVER: '',
+  OG_COVER_TWITTER: '',
+  TWITTER_HANDLE: '@mciastek',
   OG_COVER_ALT: ``,
   KEYWORDS: [
     'face detection',
